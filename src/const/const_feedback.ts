6bf8feac9e80a63328f1c75f224a69d102d8a677
ShustriyE <shutiyevgeniy@gmail.com>
2022-08-17 12:28:15 +0300

    edits commit

--- src/const/const_feedback.ts
+++ src/const/const_feedback.ts
@@ -10,30 +10,30 @@ export interface FeedbackStructure {
 
 export const feedbacks: FeedbackStructure[] = [
     {
-        nameReviewer: 'slaviogloReviewer_1',
-        positionReviewer: 'slaviogloPosition_1',
-        feedback: 'slaviogloFeedback_1',
+        nameReviewer: 'slaviogloReviewer',
+        positionReviewer: 'slaviogloPosition',
+        feedback: 'slaviogloFeedback',
         pathToImg: '/feedbackers/Slavioglo_Ivan.png',
         pathToLogoCompanyReviewer: '/feedback/Fozzy_Temabit.png'
     },
     {
-        nameReviewer: 'slaviogloReviewer_2',
-        positionReviewer: 'slaviogloPosition_2',
-        feedback: 'slaviogloFeedback_2',
+        nameReviewer: 'bonazziReviewer',
+        positionReviewer: 'bonazziPosition',
+        feedback: 'bonazziFeedback',
         pathToImg: '/feedbackers/Bonazzi_Lidiia.png',
         pathToLogoCompanyReviewer: '/feedback/Fora.png'
     },
     {
-        nameReviewer: 'slaviogloReviewer_3',
-        positionReviewer: 'slaviogloPosition_3',
-        feedback: 'slaviogloFeedback_3',
+        nameReviewer: 'kuzmenkoReviewer',
+        positionReviewer: 'kuzmenkoPosition',
+        feedback: 'kuzmenkoFeedback',
         pathToImg: '/feedbackers/Kuzmenko_Andriy.png',
         pathToLogoCompanyReviewer: '/feedback/Silpo.png'
     },
     {
-        nameReviewer: 'slaviogloReviewer_4',
-        positionReviewer: 'slaviogloPosition_4',
-        feedback: 'slaviogloFeedback_4',
+        nameReviewer: 'iosifovReviewer',
+        positionReviewer: 'iosifovPosition',
+        feedback: 'iosifovFeedback',
         pathToImg: '/feedbackers/Ievgen_Iosifov.png',
         pathToLogoCompanyReviewer: '/feedback/Ender_Turing.png'
     },
--- src/sections/SectionFooter.tsx
+++ src/sections/SectionFooter.tsx
@@ -21,77 +21,53 @@ const SectionFooter = () => {
       <div id="footer">
         <div className="footer--left">
           <img src="/icons/logo_colored.svg" alt="" />
-          {/* <ul className="footer--list">
-            <li className="footer--list-item">
-              <a href="#">First Link</a>
-            </li>
-            <li className="footer--list-item">
-              <a href="#">Second Link</a>
-            </li>
-            <li className="footer--list-item">
-              <a href="#">Third Link</a>
-            </li>
-          </ul> */}
+        </div>
+        <div className="footer--center">
+          <div className="footer--center-container">
+            <span className="center--container-header">
+              {t("contact_phone")}
+            </span>
+            <a
+              href="tel:+380666046423"
+              className="center--container-phone"
+              onClick={() => {
+                copyText(contact_phone);
+              }}
+            >
+              {contact_phone}
+            </a>
+            <span className="center--container-header">
+              {t("contact_email")}
+            </span>
+            <a
+              href="mailto:info@semiotics-lab.com"
+              className="center--container-email"
+              onClick={() => {
+                copyText(contact_email);
+              }}
+            >
+              {contact_email}
+            </a>
+            <span className="center--container-header">
+              {t("contact_adress_title")}
+            </span>
+            <a
+              href="https://www.google.com/maps/place/%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F+%D0%86%D0%BD%D1%81%D1%82%D0%B8%D1%82%D1%83%D1%82%D1%81%D1%8C%D0%BA%D0%B0,+18,+%D0%9A%D0%B8%D1%97%D0%B2,+02000/@50.44542,30.5297598,17z/data=!4m5!3m4!1s0x40d4cfacb6c2d3c7:0x6ca1da62cf62c8b6!8m2!3d50.44542!4d30.5319485"
+              target="_blank"
+              rel="noreferrer"
+              className="center--container-adress"
+            >
+              {t("contact_adress_descr")}
+            </a>
+          </div>
         </div>
         <div className="footer--right">
-          <div className="footer--right-container--left">
-            <div className="container--left-box">
-              <span className="container--left-header">
-                {t("contact_phone")}
-              </span>
-              <a
-                href="tel:+380666046423"
-                className="container--left--phone"
-                onClick={() => {
-                  copyText(contact_phone);
-                }}
-              >
-                {contact_phone}
-              </a>
-            </div>
-            <div className="container--left-box">
-              <span className="container--left-header">
-                {t("contact_email")}
-              </span>
-              <a
-                href="mailto:info@semiotics-lab.com"
-                className="container--left--email"
-                onClick={() => {
-                  copyText(contact_email);
-                }}
-              >
-                {contact_email}
-              </a>
-            </div>
-            <div className="container--left-box">
-              <span className="container--left-header">
-                {t("contact_social")}
-              </span>
-              <div className="container--left--social-box">
-                <a href="#" className="container--left--social">
-                  Twitter
-                </a>
-                <a href="#" className="container--left--social">
-                  Facebook
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="footer--right-container--right">
-            <div className="container--right-box">
-              <span className="container--right-header">
-                {t("contact_adress_title")}
-              </span>
-              <a
-                href="https://www.google.com/maps/place/%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F+%D0%86%D0%BD%D1%81%D1%82%D0%B8%D1%82%D1%83%D1%82%D1%81%D1%8C%D0%BA%D0%B0,+18,+%D0%9A%D0%B8%D1%97%D0%B2,+02000/@50.44542,30.5297598,17z/data=!4m5!3m4!1s0x40d4cfacb6c2d3c7:0x6ca1da62cf62c8b6!8m2!3d50.44542!4d30.5319485"
-                target="_blank"
-                rel="noreferrer"
-                className="container--right-adress"
-              >
-                {t("contact_adress_descr")}
-              </a>
-            </div>
-          </div>
+          <span className="footer--right-text">
+          {t("copywriting_text")}
+          </span>
+          <p className="footer--right-copywriting">
+          {t("copywriting")}
+          </p>
         </div>
       </div>
     </footer>
